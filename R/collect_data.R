fbbfb87cb659ed5b237fb26f1dda53055d4ef03e
Tom Heslop <t_heslop@hotmail.co.uk>
2016-03-13 21:20:41 +0000

    change file name and add code to collect runners and tweets

---
+++ R/collect_data.R
@@ -0,0 +1,45 @@
+setwd("C:/Users/TomHeslop/Desktop/cheltenham-festival-2016-twitter/R")
+library(streamR)
+library(magrittr)
+library(timeformR)
+
+load("../data/my_auth.RData")
+
+# login in
+tf <- timeform(usr = Sys.getenv("TF_USR"),
+               pwd = Sys.getenv("TF_PWD"))
+# todays date to get todays entries
+today <- Sys.Date()
+# retrieve entries
+entries <- tf$entries(tf_filter("meetingDate", today),
+                      tf_filter("courseId", 10),
+                      tf_filter("statusId", 21))
+# select races/runners to keep
+if(today == "2016-03-15") races <- c(1,2,4,5)
+if(today == "2016-03-16") races <- c(1,2,4)
+if(today == "2016-03-17") races <- c(1,3,4)
+if(today == "2016-03-18") races <- c(1,3,4)
+
+# create logical to test by
+test <- sapply(1:length(entries), function(i) {
+    entries[[i]]$raceNumber %in% races
+})
+# filter entries
+entries <- entries[test]
+# retrieve runners
+runners <- sapply(1:length(entries), function(i) entries[[i]]$horseName)
+# clean runner names of country suffixes, convert to lower case and add additional search terms
+runners <- runners %>%
+    tolower() %>%
+    gsub(" \\([[:alpha:]]+\\)", "", .)
+runners <- c(runners, "cheltenham", "cheltfest", "ladbrokes", "paddypower",
+             "willhill", "betvictor", "coral", "betfair", "timeform")
+
+################################################################################
+# collect tweets
+# create file name
+file_name <- paste0("../data/", today, ".json")
+filterStream(file.name = file_name,
+             track = runners,
+             timeout = 25,
+             oauth = my_oauth)
